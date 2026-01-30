06e67d7db1f2bea41f73b8c04164bf481bc78c83
iwasMateo <iwasmateo@gmail.com>
2026-01-30 21:25:06 +0100

    Change Player to PlayerStatus

--- src/main/java/org/mmcc/PlayerStatus.java
+++ src/main/java/org/mmcc/PlayerStatus.java
@@ -2,16 +2,15 @@ package org.mmcc;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Player {
+public class PlayerStatus {
     public float maxHealth = 20;
     public float currentHealth;
     public Vector3 dirSpeed;
     public float hunger;
     public Vector3 viewDir;
     public int experience;
-    public List<EffectRecord> effects;
-
-
+    public List<EffectType> effects;
+    public Vector3 size = new Vector3(0.8f, 1.8f, 0.8f);
         public void init () {
         currentHealth = maxHealth;
         dirSpeed = new Vector3(0, 0, 0);
@@ -25,5 +24,4 @@ public class Player {
     public void damage(float amount) {
         currentHealth -= amount;
     }
-
 }
