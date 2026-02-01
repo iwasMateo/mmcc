5f3997f652b88399df176b68d4d6172e6d218473
iwasMateo <iwasmateo@gmail.com>
2026-02-01 02:41:59 +0100

    Implemented Entity to Player

---
+++ src/main/java/org/mmcc/Entity.java
@@ -0,0 +1,10 @@
+package org.mmcc;
+
+public interface Entity {
+    void damage(float currentHealth);
+    float getHealth();
+    float setHealth(float health);
+    void addEffect(EffectType effect, int strength, int duration);
+    void clearEffect(EffectType effect);
+    Vector3 getDimensions();
+}
--- src/main/java/org/mmcc/PlayerStatus.java
+++ src/main/java/org/mmcc/PlayerStatus.java
@@ -2,14 +2,14 @@ package org.mmcc;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PlayerStatus {
+public class PlayerStatus implements Entity{
     public float maxHealth = 20;
     public float currentHealth;
     public Vector3 dirSpeed;
     public float hunger;
     public Vector3 viewDir;
     public int experience;
-    public List<EffectType> effects;
+    public List<EffectBehavior> effects;
     public Vector3 size = new Vector3(0.8f, 1.8f, 0.8f);
         public void init () {
         currentHealth = maxHealth;
@@ -19,6 +19,39 @@ public class PlayerStatus {
         experience = 0;
         effects = new ArrayList<>();
         IO.println("Initialised Player stats");
+    }
+
+    @Override
+    public float getHealth() {
+        return currentHealth;
+    }
+
+    @Override
+    public float setHealth(float health) {
+        currentHealth-=health;
+        return currentHealth;
+    }
+
+    @Override
+    public void addEffect(EffectType effect, int strength, int duration) {
+        effects.add(new EffectBehavior(effect, strength, duration));
+    }
+
+    @Override
+    public void clearEffect(EffectType effect) {
+        EffectBehavior found = null;
+        for (EffectBehavior e : effects) {
+            if (e.effect().id().equals(effect.id())) {
+                found = e;
+                break;
+            }
+        }
+        effects.remove(found);
+    }
+
+    @Override
+    public Vector3 getDimensions() {
+        return size;
     }
 
     public void damage(float amount) {
